8254a78de27d87912b6686b6d9ca65ca82234a44
jmanuel34 <jmanuel34@gmail.com>
2021-06-29 19:01:12 +0200

    Configuracion

--- src/main/java/com/cursoee/hoteles/lanzador/Application.java
+++ src/main/java/com/cursoee/hoteles/lanzador/Application.java
@@ -3,7 +3,10 @@ package com.cursoee.hoteles.lanzador;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestTemplate;
 
 
 //@EnableTransactionManagement
@@ -18,5 +21,5 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
+	
 }
